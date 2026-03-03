4b09038a1e51cde4e2b269618e4758557577a312
a.yusov <splash.corp@gmail.com>
2026-03-03 21:49:00 +0300

    Les 3

---
+++ Lesson 4 str.py
@@ -0,0 +1,9 @@
+from itertools import count
+
+text= 'hello world open mind'
+print(text[4]) #вытащили о из хелло
+print(len(text))
+print(text.index('el'))
+print('world' in text)
+print(text.count('o'))
+print(text[:7])
